861bdf73960f646fe868b123d78c8205a851ed06
Habeep007 <96163361+Habeep007@users.noreply.github.com>
2024-12-25 12:43:16 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tailwindPractice/', // Replace with your repo name
-  build: {
-    outDir: 'dist',
-  },
-});
+  base: process.env.BASE_URL || '/',
+})
